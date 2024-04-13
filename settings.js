c9cf30dd4f257baf4c6b1b1669d2c6196682888a
abdullatif <146804588+Boboiboyturuuu@users.noreply.github.com>
2024-04-14 01:40:38 +0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id',
+}
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': 'Your Key',-
+}
+
+//language 
+global.language = indonesia
+
+global.BOT_TOKEN = "AAGZkAXuHofADs-7Nr9itLu9xKQjZ0iwXas" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "BoboiBotzོ" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ BOBOIBOYTURUU ×፝֟͜×" //your name
+global.OWNER_NUMBER = "81243673506" //your telegram number
+global.OWNER = ["https://t.me/Boboiboyturuuu"] 
+global.THUMBNAIL = "./image/lol.jpg" 
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
